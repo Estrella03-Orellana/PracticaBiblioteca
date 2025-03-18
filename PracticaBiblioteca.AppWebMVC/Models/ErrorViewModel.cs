f582916957e8aef8cb9f544973bf12a9ad828d3a
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-03-18 14:42:57 -0600

    Agregar archivos de proyecto.

---
+++ PracticaBiblioteca.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaBiblioteca.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
