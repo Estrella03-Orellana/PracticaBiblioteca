9d81f4e6dc3545e11b3ae3db7bb489c482683e31
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-03-18 15:29:57 -0600

    Se instalaron los paqueres NuGet

---
+++ PracticaBiblioteca.AppWebMVC/Models/Biblioteca.sql
@@ -0,0 +1,50 @@
+
+CREATE DATABASE Biblioteca;
+GO
+
+USE Biblioteca;
+GO
+
+CREATE TABLE Autores (
+  Id INT PRIMARY KEY IDENTITY(1,1),
+  Nombre VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE Editoriales (
+  Id INT PRIMARY KEY IDENTITY(1,1),
+  Nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Generos (
+  Id INT PRIMARY KEY IDENTITY(1,1),
+  Nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Libros (
+  Id INT PRIMARY KEY IDENTITY(1,1),
+  Titulo VARCHAR(255) NOT NULL,
+  AutorID INT,
+  EditorialId INT,
+ FOREIGN KEY (AutorId) REFERENCES Autores(Id),
+ FOREIGN KEY (EditorialId) REFERENCES Editoriales(Id)
+);
+
+
+CREATE TABLE Libros_Generos(
+    LibroId INT,
+    GeneroId INT,
+    PRIMARY KEY (LibroId, GeneroId),
+    FOREIGN KEY (LibroId) REFERENCES Libros(Id),
+    FOREIGN KEY (GeneroId) REFERENCES Generos(Id)
+);
+
+CREATE TABLE Usuarios (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Nombre VARCHAR(255) NOT NULL,
+    Password CHAR(32) NOT NULL,
+    Email VARCHAR(255) UNIQUE NOT NULL,
+    FechaRegistro DATETIME DEFAULT GETDATE(),
+    UltimoInicioSesion DATETIME,
+    Estatus TINYINT DEFAULT 1 -- Usamos TINYINT para Estatus y un valor predeterminado de 1
+);
+
