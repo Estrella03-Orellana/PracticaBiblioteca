da9d7dbd85edc227b0d77c226c1a96ddb4322a6f
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-03-18 16:02:17 -0600

    Se crearon los controllers y vistas

---
+++ PracticaBiblioteca.AppWebMVC/Controllers/LibroController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PracticaBiblioteca.AppWebMVC.Models;
+
+namespace PracticaBiblioteca.AppWebMVC.Controllers
+{
+    public class LibroController : Controller
+    {
+        private readonly BibliotecaContext _context;
+
+        public LibroController(BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Libro
+        public async Task<IActionResult> Index()
+        {
+            var bibliotecaContext = _context.Libros.Include(l => l.Autor).Include(l => l.Editorial);
+            return View(await bibliotecaContext.ToListAsync());
+        }
+
+        // GET: Libro/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.Libros
+                .Include(l => l.Autor)
+                .Include(l => l.Editorial)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        // GET: Libro/Create
+        public IActionResult Create()
+        {
+            ViewData["AutorId"] = new SelectList(_context.Autores, "Id", "Id");
+            ViewData["EditorialId"] = new SelectList(_context.Editoriales, "Id", "Id");
+            return View();
+        }
+
+        // POST: Libro/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Titulo,AutorId,EditorialId")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(libro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AutorId"] = new SelectList(_context.Autores, "Id", "Id", libro.AutorId);
+            ViewData["EditorialId"] = new SelectList(_context.Editoriales, "Id", "Id", libro.EditorialId);
+            return View(libro);
+        }
+
+        // GET: Libro/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.Libros.FindAsync(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            ViewData["AutorId"] = new SelectList(_context.Autores, "Id", "Id", libro.AutorId);
+            ViewData["EditorialId"] = new SelectList(_context.Editoriales, "Id", "Id", libro.EditorialId);
+            return View(libro);
+        }
+
+        // POST: Libro/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titulo,AutorId,EditorialId")] Libro libro)
+        {
+            if (id != libro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(libro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LibroExists(libro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AutorId"] = new SelectList(_context.Autores, "Id", "Id", libro.AutorId);
+            ViewData["EditorialId"] = new SelectList(_context.Editoriales, "Id", "Id", libro.EditorialId);
+            return View(libro);
+        }
+
+        // GET: Libro/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.Libros
+                .Include(l => l.Autor)
+                .Include(l => l.Editorial)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        // POST: Libro/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var libro = await _context.Libros.FindAsync(id);
+            if (libro != null)
+            {
+                _context.Libros.Remove(libro);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LibroExists(int id)
+        {
+            return _context.Libros.Any(e => e.Id == id);
+        }
+    }
+}
